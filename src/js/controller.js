926c3c23974d871073967a71482d8ec2ebefcf0c
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2025-11-11 23:20:11 +0530

    add settings view, add functionality for tab change

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,16 +1,29 @@
-import {createIcons, icons} from 'lucide';
-import * as Model from '../js/Views/settingsView';
-import dashboardView from './Views/dashboardView';
+import { createIcons, icons } from "lucide";
+import * as Model from "../js/Views/settingsView";
+import dashboardView from "./Views/dashboardView";
+import settingsView from "./Views/settingsView";
 
+const controlNavigation = function (targetPage) {
+//   console.log(targetPage);
+setUrl(targetPage);
+if(targetPage === 'settings')
+{
+    settingsView.renderView();
+    settingsView.navigateTabs();
+}
+};
 
-const controlNavigation = function(targetPage){
-    console.log(targetPage);
+const setUrl = function(targetUrl){
+    const url = new URL(window.location.href);
+    const newUrl = `${url.origin}/${targetUrl}`;
+    // console.log(newUrl);
+    history.pushState({},'',newUrl);
 }
 
 
-const init = function(){
-createIcons({icons});
-dashboardView.addHandlerNavigatePage(controlNavigation);
-}
+const init = function () {
+  createIcons({ icons });
+  dashboardView.addHandlerNavigatePage(controlNavigation);
+};
 
 init();
